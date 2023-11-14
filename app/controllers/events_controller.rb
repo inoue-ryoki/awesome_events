0a6515e76f0172a1fc751d32c006af3549078788
inoue-ryoki <mth.1021@icloud.com>
2023-11-14 21:49:02 +0900

    削除と編集一応動いた

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,12 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate, only: :show
+
+  def show
+    @event = Event.find(params[:id])
+
+    @current_event = Event.where(owner_id: current_user.id)
+  end
+
   def new
     @event = current_user.created_events.build
   end
@@ -8,6 +16,32 @@ class EventsController < ApplicationController
 
     if @event.save
       redirect_to @event, notice: "作成しました"
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to root_path}
+      else
+        format.html { render :edit}
+        format.js
+      end
+    end
+
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.present?
+      @event.destroy
+      redirect_to root_path
     end
   end
 
